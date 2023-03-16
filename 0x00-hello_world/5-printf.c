4e15b5a2b66c54eec64292253587e79410b6a930
Jymy <basmajymy@gmail.com>
2023-03-16 10:30:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	ptintf("with proper grammar, but the outcome is a piece of art,\n";
-	return(0);
+	ptintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
