8aff95a26f06a7ea89853d750246578f8c0b281e
Jymy <basmajymy@gmail.com>
2023-03-22 15:33:36 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,13 @@
 
 /**
  * print_last_digit - function 'print_last_digit'
- *@i : parameter
+ *@n : parameter
  * Return: lastDigit
  */
 int print_last_digit(int n)
 {
 int lastDigit;
-if (i < 0)
+if (n < 0)
 lastDigit = -1 * (n % 10);
 else
 lastDigit = n % 10;
