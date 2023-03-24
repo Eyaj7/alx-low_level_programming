8c129c08b1e6fd5fa6855d23d2068dae3a46d122
Jymy <basmajymy@gmail.com>
2023-03-24 15:46:32 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,9 @@ if (i % 3 == 0 && i % 5 != 0)
 printf("Fizz ");
 else if (i % 5 == 0 && i % 3 != 0)
 printf("Buzz ");
-else if (i % 5 == 0 && i % 3 != 0)
+else if (i % 3 == 0 && i % 5 != 0)
+printf("FizzBuzz ");
+else if (i % 5 == 0 && i % 3 == 0)
 printf("FizzBuzz ");
 else if (i == 1)
 printf("%d ", i);
