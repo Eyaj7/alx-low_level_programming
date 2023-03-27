3808ea0127e673d276f86de134839528d217e0a5
Jymy <basmajymy@gmail.com>
2023-03-27 08:09:09 -0700

    6-main.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev -  function that prints a string
+ * @s: string
+ * Return: Always(0)
+ */
+void print_rev(char *s);
+int longi = 0;
+
+while (*s)
+{
+s++;
+count++;
+}
+
+while (count)
+{
+s--;
+_putchar(*s);
+count--;
+}
+
+_putchar('\n');
+}
