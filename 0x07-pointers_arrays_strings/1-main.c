674988dd8e170113c80c6b2ddd13f7fd304a65fb
Jymy <basmajymy@gmail.com>
2023-04-03 17:08:06 -0700

    1-main.c

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
+* simple_print_buffer - prints buffer in hexa
+* @buffer: the address of memory to print
+* @size: the size of the memory to print
+*
+* Return: Nothing.
+*/
 void simple_print_buffer(char *buffer, unsigned int size)
 {
 unsigned int i;
-
 i = 0;
 while (i < size)
 {
@@ -40,6 +39,6 @@ char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 simple_print_buffer(buffer, 98);
 _memcpy(buffer + 50, buffer2, 10);
 printf("-------------------------------------------------\n");
-simple_print_buffer(buffer, 98);    
+simple_print_buffer(buffer, 98);
 return (0);
 }
