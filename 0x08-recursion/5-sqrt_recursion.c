fa88d9170c4e7e1498516ce1882ef0a0638b5037
Jymy <basmajymy@gmail.com>
2023-04-06 19:07:43 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,27 @@
 #include "main.h"
+/**
+ * check - function 'check'
+ * @y:int
+ * @n:int
+ * Return: Always(0)
+ */
+int check(int n, int y)
+{
+if (y * y > n)
+return (-1);
+if (y * y == n)
+return (y);
+return (check(n, y + 1));
+}
 
 /**
  *_sqrt_recursion - function '_sqrt_recursion'
- *@x:int
- *@y:int
+ *@n:int
  *Return: Always(0)
  */
 int _sqrt_recursion(int n)
 {
-
+if (n < 0)
+return (-1);
+return (check(n, 0));
+}
