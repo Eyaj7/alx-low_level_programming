3635be26e98797b3b2f256ab4929ea2cd2a2fc84
Jymy <basmajymy@gmail.com>
2023-04-11 09:46:48 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints
+ *@args: int
+ *@argv: int
+ *Return: Always (0)
+ **/
+int main(int args, char *argv[])
+{
+(void)argv;
+printf("%d\n", args - 1);
+return (0);
+}
