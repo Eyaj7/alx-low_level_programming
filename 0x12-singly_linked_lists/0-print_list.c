c4c2f6b5fff7d6287113e086161610f797a158cc
Jymy <basmajymy@gmail.com>
2023-04-27 08:56:15 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
 
 size_t print_list(const list_t *h)
 {
-int i = 0;
+int s = 0;
 while (h)
 {
 if (h->str == NULL)
@@ -25,5 +25,5 @@ printf("[%d) %s\n", h->len, h->str);
 i++;
 h = h->next;
 }
-return (i);
+return (s);
 }
