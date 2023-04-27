50d42ba0a12f638a930a8141190ebaa6404b546b
Jymy <basmajymy@gmail.com>
2023-04-27 10:43:21 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - function
+ * @h: int
+ * Return: Always (0)
+ */
+size_t list_len(const list_t *h)
+{
+int count = 0;
+while (h)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
