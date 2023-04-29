1327c39ab0f5d4b6fd9348e9f4c7064288a1254f
Jymy <basmajymy@gmail.com>
2023-04-29 11:20:21 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - function
+ * @head: pointer
+ * Return: Always(0)
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *t;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		t = (*head)->next;
+		free(*head);
+		*head = t;
+	}
+	head = NULL;
+}
