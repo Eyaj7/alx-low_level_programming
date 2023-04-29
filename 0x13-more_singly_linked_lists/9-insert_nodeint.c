e09712a07304cec63f8538140b1b0db9f10e690c
Jymy <basmajymy@gmail.com>
2023-04-29 16:36:31 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (idx != 0)
 	{
-		for (node = 0; node < idx - 1 || t != NULL; node++)
+		for (node = 0; node < idx - 1 && t != NULL; node++)
 		{
 			t = t->next;
 		}
