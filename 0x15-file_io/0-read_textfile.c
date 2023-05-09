9cc86ead276628209f69bceefa317000809a18bf
Jymy <basmajymy@gmail.com>
2023-05-09 10:36:46 -0700

    Requiescat

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t o, r, w;
 
+	if (filename == NULL)
+	{
+		return (0);
+	}
 	o = open(filename, O_RDONLY);
+	if (o == -1)
+	{
+		return (0);
+	}
 	buffer = malloc(sizeof(char) * letters);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
