bf95c08cba81dc9c0711fe394950d7d8952e83fc
Jymy <basmajymy@gmail.com>
2023-05-09 13:11:37 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, l = 0;
+	int o, w, s = 0;
 
 	if (filename == NULL)
 	{
@@ -19,12 +19,12 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (l = 0; text_content[l];)
-			l++;
+		for (s = 0; text_content[s];)
+			s++;
 	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, l);
-	if (o == -1 || w == -l)
+	w = write(o, text_content, s);
+	if (o == -1 || w == -1)
 	{
 		return (-1);
 	}
